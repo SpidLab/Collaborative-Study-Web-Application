9569ecc6c021b2762ae3ab99ec357c8b3f9642c6
fatihahmetg <fatihahmet06@hotmail.com>
2023-06-17 10:27:52 -0700

    Chenages

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,6 @@ import logger from "./utils/logger"
 import middleware from "./utils/middleware"
 import config from "./utils/config"
 import mongoose from "mongoose"
-import credentialsRouter from "./controllers/credentialsController"
-import employeeRouter from "./controllers/employeeController"
-import healthDataRouter from "./controllers/healthDataController"
-import pdaRouter from "./controllers/pdaController"
-import userAgentRouter from "./controllers/userAgentUsageController"
 
 logger.info('---')
 logger.info('Connecting to MongoDB')
@@ -29,27 +24,10 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-// Authentication routes
+// Routes
 // ==============================================
 
-app.use('/api/credentials', credentialsRouter)
 
-// Get health data routes
-// ==============================================
-
-app.use('/api/health-data', healthDataRouter)
-
-// DB routes
-// ==============================================
-
-app.use('/api/employee', employeeRouter)
-
-// PDA routes
-// ==============================================
-app.use('/api/pda', pdaRouter)
-
-// Interaction with User Agent Controller routes
-app.use('/api/user-agent-controller', userAgentRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
