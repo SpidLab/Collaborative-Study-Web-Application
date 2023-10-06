2238fb80bd115ed4a9b1ae3115738983e76e60bb
fatihahmetg <fatihahmet06@hotmail.com>
2023-10-06 18:07:55 -0400

    Added the secret key field to config.ts to be used by JWT

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -3,9 +3,12 @@ dotenv.config()
 
 const PORT = process.env.PORT as string
 const MONGODB_URI = process.env.MONGODB_URI as string
+const SECRET = "bezkoder-secret-key" as string
+
 const config = {
     PORT,
     MONGODB_URI,
+    SECRET
 }
 
 export default config
