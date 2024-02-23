bd7fddc682c7ad8107068f2f443554b63531dffb
Alexander Nemecek <an440818@ohio.edu>
2024-02-23 11:45:55 -0500

    Frontend Google SignIn Functional

--- web_application/Frontend/src/main.jsx
+++ web_application/Frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <GoogleOAuthProvider clientId='370001197002-kpmn78q6r8urks4sgv7vhbp7s09366q5.apps.googleusercontent.com'>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </GoogleOAuthProvider>
 )
