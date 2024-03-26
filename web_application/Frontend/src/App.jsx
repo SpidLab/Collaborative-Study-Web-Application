a82fd60ea7b5005fc6d32097c46b9ef3343a2cce
smit2444 <157842055+smit2444@users.noreply.github.com>
2024-03-26 20:56:32 +0000

    Added MUI Styling and Routing to Application

--- web_application/Frontend/src/App.jsx
+++ web_application/Frontend/src/App.jsx
@@ -1,15 +1,32 @@
-import "./App.css";
-import {Navigation} from "./components";
-import {Search} from "./components";
-import {Upload} from "./components";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { Upload, Search } from "./components";
+import BrightnessHighIcon from '@mui/icons-material/BrightnessHigh';
 
 function App() {
   return (
-    <>
-      <Navigation />
-      <Search />
-      <Upload />
-    </>
+    <Router>
+      <AppBar position="static">
+        <Toolbar>
+          <BrightnessHighIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Collabrative Web Application
+          </Typography>
+          <Button color="inherit" component={RouterLink} to="/">
+            Upload
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/search">
+            Search
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Routes>
+        <Route index element={<Upload />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Router>
   );
 }
 
--- web_application/Frontend/src/components/Search/Search.jsx
+++ web_application/Frontend/src/components/Search/Search.jsx
@@ -1,23 +1,49 @@
-import './Search.css';
+import React from 'react';
+import { Container, Grid, TextField, Button, Typography } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 function SearchPage() {
-    return (
-        <div className="search-container">
-            <div className="left-column">
-            <label className='sec_title'>Initiator Search</label>
-                <div className="search-block">
-                    <label>Phenotype(s)</label>
-                    <input type="text"/>
-                    <label>Minimum # of Samples</label>
-                    <input type="text"/>
-                    <button>Search For Collabrator</button>
-                </div>
-            </div>
-            <div className="right-column">
-                <p>Search Results</p>
-            </div>
-        </div>
-    );
+  return (
+    <Container component="div" maxWidth="md" sx={{ marginTop: 4 }}>
+      <Grid container spacing={3} alignItems="center" justifyContent="center">
+        <Grid item xs={12}>
+          <Typography variant="h5" align="center" gutterBottom>
+            Initiator Search
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            fullWidth
+            label="Phenotype(s)"
+            variant="outlined"
+            margin="normal"
+            sx={{ marginBottom: 2 }}
+          />
+          <TextField
+            fullWidth
+            label="Minimum # of Samples"
+            variant="outlined"
+            margin="normal"
+            sx={{ marginBottom: 2 }}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<SearchIcon />}
+            fullWidth
+          >
+            Search For Collaborator
+          </Button>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h6" align="center" gutterBottom>
+            Search Results
+          </Typography>
+          {/* Placeholder for search results */}
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default SearchPage;
