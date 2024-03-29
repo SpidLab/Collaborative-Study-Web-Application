1d6f8a6686313b70666b205b4f40054fc31054a5
Alexander Nemecek <alexandernemecek@Alexanders-Mac-mini.local>
2024-03-29 12:53:47 -0400

    Added Session page

--- web_application/Frontend/src/components/index.js
+++ web_application/Frontend/src/components/index.js
@@ -2,5 +2,6 @@
 import Navigation from "./Navigation/Navigation";
 import Search from "./Search/Search";
 import Upload from "./Upload/Upload";
+import Session from "./Session/Session"
 
-export { Navigation, Search, Upload };
+export { Navigation, Search, Upload, Session };
