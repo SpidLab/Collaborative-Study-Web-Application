d81292cda80f269f5b1a36069a8e9a28e426f77c
maisoonrahman <maisoon.a.r01@gmail.com>
2024-03-29 03:36:10 -0400

    added csv endpoint

--- web_application/Backend/FlaskApp/app.py
+++ web_application/Backend/FlaskApp/app.py
@@ -8,6 +8,10 @@ from bson.json_util import dumps
 import json
 import os
 from dotenv import load_dotenv, find_dotenv
+import pandas as pd
+from werkzeug.utils import secure_filename
+import os
+
 
 app = Flask(__name__)
 load_dotenv(find_dotenv())
@@ -93,5 +97,33 @@ def get_users():
     return dumps(users_list), 200
 
 
+@app.route('/api/uplaod_csv', methods=['POST'])
+@login_required
+def upload_csv():
+    # Check if the post request has the file part
+    if 'file' not in request.files:
+       return jsonify({'message': 'No file part in the request'}), 400
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an empty file w/o a filename
+    if file.filename == '':
+        return jsonify({'message': 'No selected file'}), 400
+    if file and file.filename.endswith('.csv'):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join('/tmp', filename)
+        file.save(filepath)
+        try:
+            df = pd.read_csv(filepath)
+                # calculate coefficients after upload
+
+            return jsonify({'message': 'CSV file processed successfully'}), 200
+        except Exception as e:
+            return jsonify({'message': 'An error occurred while processing the file', 'error': str(e)}), 500
+    else:
+        return jsonify({'message': 'Unsupported file type'}), 400
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
